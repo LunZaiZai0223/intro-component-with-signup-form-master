2a35d4ae4ca13f4dcc06887e26bb3ceed5b94abd
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-04-09 20:43:35 +0800

    handle deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/intro-component-with-signup-form-master/",
 });
